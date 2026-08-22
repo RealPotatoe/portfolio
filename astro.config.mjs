929a18ceac913fb679e30a3c04467cce443c29e1
RealPotatoe <bengel_moritz@gmx.de>
2026-08-22 09:54:02 +0200

    feat: generate sitemap during builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://mbengel.dev",
-    integrations: [svelte()],
+    integrations: [svelte(), sitemap()],
     i18n: {
         locales: ["en", "de"],
         defaultLocale: "en",
